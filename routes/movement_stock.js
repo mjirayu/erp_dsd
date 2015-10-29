b3d26c88c31aa1dc2689738665dba8ad142be91b
Suthat  Thanajan <suthat.th@hotmail.com>
2015-10-30 00:25:42 +0700

    Fix search by peroid

--- routes/movement_stock.js
+++ routes/movement_stock.js
@@ -30,32 +30,59 @@ router.get('/movement/search/date', function(req, res, next) {
       movement_type: { $regex: movement_type },
       update_date : {
         $gte: date_from,
-        $lt: date_to
+        $lte: date_to
       }
     }.function(err, data) {
       res.send(data);
     });
 });
 
-/*
+
 router.get('/movement/search/period', function(req, res, next) {
+  /*var lastWeek = new Date();
+  lastWeek.setDate(lastWeek.getDate() -7);*/
   var params = req.query;
   var movement_type = new RegExp(params.transaction_type, 'i');
-  var date_from = new Date(params.date_from);
-  var date_to = new Date(params.date_to);
+  var date_to = new Date();
+  var date_from = new Date();
   var pd_id = params.pd_id;
+  var period = params.period;
+  var check_date = false;
 
-  movementModel.find({
-      pd_id: pd_id,
-      movement_type: { $regex: movement_type },
-      update_date : {
-        $gte: date_from,
-        $lt: date_to
-      }
-    }.function(err, data) {
-      res.send(data);
+  if(period == "today") {
+    check_date = true;
+  } else if (period == "7days") {
+    date_from.setDate(date_from.getDate() -7);
+    check_date = true;
+  } else if (period == "1month") {
+    date_from.setDate(date_from.getDate() -30);
+    check_date = true;
+  }else if (period == "3months") {
+    date_from.setDate(date_from.getDate() -90);
+    check_date = true;
+  }else if (period == "6months") {
+    date_from.setDate(date_from.getDate() -180);
+    check_date = true;
+  }else if (period == "1year") {
+    date_from.setDate(date_from.getDate() -365);
+    check_date = true;
+  }else {
+    res.send('Period is not match :'+period);
+  }
+
+  if(check_date){}
+    movementModel.find({
+        pd_id: pd_id,
+        movement_type: { $regex: movement_type },
+        update_date : {
+          $gte: date_from,
+          $lte: date_to
+        }
+      }.function(err, data) {
+        res.send(data);
+      });
     });
-});
-*/
+  }
+
 
 module.exports = router;
