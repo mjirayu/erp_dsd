976740da93f8678aa78644d10e97bf19f090cb78
tostarter <tostarter@gmail.com>
2015-11-01 19:14:47 +0700

    add required message to m_product, po_header, po_transaction model :cat:

--- models/m_product.js
+++ models/m_product.js
@@ -2,16 +2,43 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var productSchema = new Schema({
-  pd_id: String,
-  pd_name: String,
-  pd_status: String,
-  pd_type: String,
-  safety_stock: Number,
-  unit: String,
-  image: String,
-  update_date: Date,
-  update_by: String,
+  pd_id: {
+    type: String,
+    unique: true,
+    required: 'pd_id is required!',
+  },
+  pd_name: {
+    type: String,
+    required: 'pd_name is required!',
+  },
+  pd_status: {
+    type: String,
+    required: 'pd_status is required!',
+  },
+  pd_type: {
+    type: String,
+    required: 'pd_type is required!',
+  },
+  safety_stock: {
+    type: Number,
+    required: 'safety_stock is required!',
+  },
+  unit: {
+    type: String,
+    required: 'unit is required!',
+  },
+  image: {
+    type: String,
+    required: 'image is required!',
+  },
+  update_date: {
+    type: Date,
+    required: 'update_date is required!',
+  },
+  update_by: {
+    type: String,
+    required: 'update_by is required!',
+  },
 });
-
 
 module.exports = mongoose.model('M_PRODUCT', productSchema);
