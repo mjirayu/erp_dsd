0f13d49e73a1bd47b199bc34f7e625e8eba83ac8
tostarter <tostarter@gmail.com>
2015-12-06 14:11:31 +0700

    change product model and product route :cat:

--- models/m_product.js
+++ models/m_product.js
@@ -5,40 +5,32 @@ var Schema = mongoose.Schema;
 var productSchema = new Schema({
   pd_id: {
     type: String,
-    unique: 'This pd_id is already exist',
-    required: 'pd_id is null.',
   },
   pd_name: {
     type: String,
-    required: 'pd_name is null.',
   },
   pd_status: {
     type: String,
-    required: 'pd_status is null.',
   },
   pd_type: {
     type: String,
-    required: 'pd_type is null.',
   },
   safety_stock: {
     type: Number,
-    required: 'safety_stock is null.',
+    default: null,
   },
   unit: {
     type: String,
-    required: 'unit is null.',
+    default: null,
   },
   image: {
     type: String,
-    required: 'image is null.',
   },
   update_date: {
     type: String,
-    required: 'update_date is null.',
   },
   update_by: {
     type: String,
-    required: 'update_by is null.',
   },
 });
 
